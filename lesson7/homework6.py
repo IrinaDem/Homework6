0acb831e2b1cf5310928fe53d1fee44bf87dd1e0
IrinaDem <starshinova9297@gmail.com>
2024-07-27 19:05:08 +0300

    Add files via upload

---
+++ lesson7/homework6.py
@@ -0,0 +1,14 @@
+my_dict={'Irina': 1997,'Alex': 1992}
+print(my_dict)
+print(my_dict ['Alex'])
+print(my_dict.get('Kate'))
+my_dict.update({'Viktoria': 2021,
+'Ulia': 1975})
+print(my_dict)
+h = my_dict.pop('Irina')
+print(h)
+my_set= {3,3,4,5,1,2,3,4,5}
+print(my_set)
+my_set= {3,3,4,5,1,2,3,4,5,'Cherry',(7,8,9)}
+print(my_set.discard(3))
+print(my_set)
